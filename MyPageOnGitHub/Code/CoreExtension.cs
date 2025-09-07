3f5ec1ca93e6008e2ec70ff8e20fc4573cb41b30
Caspi N. <github@caspi.de>
2025-09-07 14:12:44 +0200

    reorga

---
+++ MyPageOnGitHub/Code/CoreExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace MyPageOnGitHub.Code;
+
+public static class CoreExtension
+{
+    public static WebAssemblyHostBuilder AddCore(this WebAssemblyHostBuilder @this)
+    {
+        @this.Logging
+            .SetMinimumLevel(LogLevel.Information)
+            .AddFilter("Microsoft", LogLevel.Warning)
+            // .AddFilter("Microsoft", LogLevel.Information)
+            .AddFilter("System.Net.Http.HttpClient", LogLevel.Warning);
+
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json")
+            .Build();
+
+        @this.Configuration.AddConfiguration(configuration);
+
+        return @this;
+    }
+}
--- MyPageOnGitHub/Program.cs
+++ MyPageOnGitHub/Program.cs
@@ -9,17 +9,30 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Logger, IConfiguration etc.
+builder.AddCore();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMudServices();
+
+// MarkdownService
 builder.Services.AddCoreServices();
 
-builder.Services.AddMudServices();
 builder.Services.AddMudMarkdownServices();
 // Optionally if the default clipboard functionality fails it is possible to add a custom service
 // NB! MauiClipboardService is just an example
 // builder.Services.AddMudMarkdownClipboardService<MauiClipboardService>();
 
+var serviceProvider = builder.Services.BuildServiceProvider();
+var logger = serviceProvider.GetRequiredService<ILoggerFactory>().CreateLogger<Program>();
 
+var app = builder.Build();
 
-await builder.Build().RunAsync();
+logger.LogInformation("AppDomain.CurrentDomain.BaseDirectory: {name}", AppDomain.CurrentDomain.BaseDirectory);
+logger.LogInformation("client environment: {name}", builder.HostEnvironment.Environment);
+logger.LogInformation(builder.Configuration["appSettings:Environment"] ?? "no appSettings found");
+
+await app.RunAsync();
+
 
