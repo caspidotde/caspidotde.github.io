eb3becb875a53aee711929f77760d25ad2aa9d63
Caspi N. <github@caspi.de>
2026-02-15 02:06:46 +0100

    circle indexes

--- MyPageOnGitHub/Code/MarkdownService.cs
+++ MyPageOnGitHub/Code/MarkdownService.cs
@@ -54,20 +54,26 @@ public class MarkdownService: IMarkdownService
 
     public async Task<string> GetBlogEntryAsync(MarkdownResourceType resourceType, int itemIndex)
     {
-        if (itemIndex < 1 || itemIndex > 999)
+        var tree = await GetContentTree();
+        var currentNode = tree.Nodes
+            .Where(w => w.Name.Equals(getResourceDirectory(resourceType)))
+            .FirstOrDefault();
+
+        if (currentNode == null)
         {
-            var tree = await GetContentTree();
-            var currentNode = tree.Nodes
-                .Where(w => w.Name.Equals(getResourceDirectory(resourceType)))
-                .FirstOrDefault();
+            return await GetBlogEntryAsync(resourceType, 1);
+        }
 
-            if (currentNode == null)
-            {
-                return await GetBlogEntryAsync(resourceType, 1);
-            }
+        var lastIndex = currentNode.Children.Max(m => m.Index);
 
-            var lastIndex = currentNode.Children.Max(m => m.Index);
+        if (itemIndex < 1)
+        {
             return await GetBlogEntryAsync(resourceType, lastIndex);
+        }
+
+        if (itemIndex > lastIndex)
+        {
+            return await GetBlogEntryAsync(resourceType, 1);
         }
 
         var resourceDirectory = getResourceDirectory(resourceType);
